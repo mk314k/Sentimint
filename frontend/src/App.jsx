defd86d8abd1e2007ed3729fc2092b4f1ef1992c
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2024-02-16 15:17:19 -0500

    setting up react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex-vertical'>Google PM Hackathon 2024</div>
+      <div>Under Development</div>
+       
+    </>
+  )
+}
+
+export default App
