67583c12e207e124b2f26da348ee5a34a79d75d1
Annie Zhang <zz229@cornell.edu>
2024-02-19 23:50:37 -0500

    cleanup homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import Menu from './Menu.jsx';
 import Overview from './Overview.jsx';
+import Search from './Search.jsx';
 import MagnifyingGlassIcon from './icons/magnifying-glass.svg';
 import CogIcon from './icons/cog.svg';
 import BellIcon from './icons/bell.svg';
@@ -13,13 +14,7 @@ function App() {
         <>
             <Menu />
             <div className='content'>
-                <div className='search'>
-                    <img src={MagnifyingGlassIcon} className='search__icon' />
-                    <span className='search__text'>Search</span>
-                    <img src={BellIcon} className='search__icon--right' />
-                    <img src={CogIcon} className='search__icon--right' />
-                    <img src={LifeAlive} className='search__icon--right' />
-                </div>
+                <Search />
                 <Overview />
             </div>
         </>
